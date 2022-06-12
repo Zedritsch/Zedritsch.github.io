e742e69f7025c13f2eebe8e6ae835087d3272d75
Zedritsch <cedric.buttkus@gmail.com>
2022-06-12 16:19:07 +0200

    7

--- js/loader.js
+++ js/loader.js
@@ -1,31 +1,28 @@
 const MAIN = document.getElementsByTagName("main")[0];
 
-onload = () => {
-	switch(location.pathname) {
-		case "/":
-			MAIN.innerHTML = request("html/index.html");
-			break;
-		default:
-			MAIN.innerHTML = request("html/404.html")
-	}
+onload = async () => {
+	(location.pathname == "/")?
+		MAIN.innerHTML = await request("html/index.html")
+	: (check("html" + location.pathname + ".html"))?
+		MAIN.innerHTML = await request("html" + location.pathname + ".html")
+	: MAIN.innerHTML = await request("html/404.html")
 	switch(navigator.language) {
 		case "de-DE":
-			translate(request("lang/de.json"));
+			translate(await request("lang/de.json"));
 		default:
 			document.documentElement.setAttribute("lang", "en-US")
 	}
-	scrollTo({
+	/*scrollTo({
 		behavior: "smooth",
 		top:
 			document.getElementById(location.hash).getBoundingClientRect().top -
 			document.body.getBoundingClientRect().top -
 			2.8 * parseFloat(getComputedStyle(document.getElementsByTagName("header")[0]).fontSize)
-	})
+	})*/
 }
 
-function request(url) {
-	return fetch(url).then(res => res.text())
-}
+async function check(url) { return fetch(url, {method: "HEAD"}).then(res => res.ok) }
+async function request(url) { return await (await fetch(url)).text() }
 
 function translate(json) {
 	json = JSON.parse(json);
