5d347536a5b12505e1e854772f06d2c2410170ff
Zedritsch <cedric.buttkus@gmail.com>
2022-06-13 08:02:06 +0200

    09

--- js/loader.js
+++ js/loader.js
@@ -1,16 +1,10 @@
-const A=["de"];async function b(c){
-	const D=navigator.language.substring(0,2);
-	if(A.includes(D)){
-		const E=JSON.parse(await (fetch("/lang/"+D+c+".json").then(f=>f.text)));
-		for(let g in E)document.getElementById(g).innerHTML=E[g];
-		document.documentElement.setAttribute("lang",navigator.language)
-	}
-	else document.documentElement.setAttribute("lang","en-US");
-}function c(){
-	if(document.documentElement.getAttribute("lang")==null)b("");
-	if(location.pathname=="/")location.pathname="/home";
-	fetch("/html"+location.pathname).then(d=>{
-		if(d.ok)d.text;
-		else{location.pathname="/404";
-		rel()}
-	}).then(e=>{document.getElementsByTagName("main")[0].innerHTML=e;b(location.pathname)});scrollTo({behavior:"smooth",top:0/*TODO*/})}onload=c
+const A=navigator.language.substring(0,2);const B=["de"].includes(A);const C=document.documentElement;const D=document.getElementsByTagName("main")[0];async function e(c){const F=JSON.parse(await (fetch(`/lang/${A+c}.json`).then(g=>g.text)));for(let h in F)document.getElementById(h).innerText=F[h];}function f(path){
+	if(path=="/")path="/home";
+	fetch("/html"+path).then(g=>{
+		if(g.ok)return g.text;
+		f("/404");
+		return null
+	}).then(h=>{
+		D.innerHTML=h;
+		if(B)e(path)
+	});scrollTo({behavior:"smooth",top:0/*TODO*/})}if(C.getAttribute("lang")==null){if(B){C.setAttribute("lang",A);e("")}else C.setAttribute("lang","en")};onload=f(location.pathname)
